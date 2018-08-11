0be6f7c2523c2b432e1f0f4c10dae9f89e467045
John Mairs <John.D.Mairs@gmail.com>
2018-08-11 14:28:45 -0400

    removed build

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/jmairs/Documents/_sqlite
-# Build directory: /Users/jmairs/Documents/_sqlite/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
