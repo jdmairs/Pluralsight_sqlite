4994ad3edd803b6c5cb02f0f6cc576dd04cc17e3
John Mairs <John.D.Mairs@gmail.com>
2018-08-11 19:46:54 -0400

    connection_handle works

--- handle.h
+++ handle.h
@@ -2,7 +2,7 @@
 
 #ifdef _DEBUG
 #include <crtdbg.h>
-#define ASSERT ASSERTE
+#define ASSERT _ASSERTE
 #else
     #define ASSERT __noop
 #endif
@@ -16,20 +16,108 @@ struct HandleTraits
     {
         return nullptr;
     }
+    // static void Close(Type value) noexcept
 
 };
 
 template <typename Traits>
-struct HandleTraits
+class Handle
 {
     using Type=decltype(Traits::Invalid());
     Type mValue;
 
-    void close() noexcept
+    void Close() noexcept
     {
         if (*this)
         {
-            Traits::close(mValue);
+            Traits::Close(mValue);
         }
     }
+
+    public:
+
+    Handle(Handle const &) = delete;
+    Handle & operator=(Handle const&) = delete;
+
+    explicit Handle(Type value = Traits::Invalid()) noexcept:
+        mValue(value)
+        {}
+
+    Handle(Handle && other) noexcept:
+    mValue(other.Detach())
+    {}
+    
+    Handle & operator=(Handle && other) noexcept
+    {
+        if (this != &other)
+        {
+            Reset(other.Detach());
+        }
+
+        return *this;
+    }
+
+    ~Handle() noexcept
+    {
+        Close();
+    }
+
+    explicit operator bool() const noexcept
+    {
+        return mValue != Traits::Invalid();
+    }
+
+    Type Get() const noexcept
+    {
+        return mValue;
+    }
+
+    Type * Set() noexcept
+    {
+        ASSERT(!*this);
+        return &mValue;
+    }
+
+    Type Detach() noexcept
+    {
+        Type value = mValue;
+        mValue = Traits::Invalid();
+        return value;
+    }
+
+    bool Reset(Type value = Traits::Invalid()) noexcept
+    {
+        if (mValue != value)
+        {
+            Close();
+            mValue = value;
+        }
+
+        return static_cast<bool>(*this);
+    }
+
+    void Swap(Handle<Traits> & other) noexcept
+    {
+        Type temp = mValue;
+        mValue = other.mValue;
+        other.mValue = temp;
+    }
 };
+
+template <typename Traits>
+void swap(Handle<Traits>& left, Handle<Traits>& right) noexcept
+{
+    left.Swap(right);
+}
+
+template <typename Traits>
+bool operator==(Handle<Traits> const & left, Handle<Traits> const & right) noexcept
+{
+    return left.Get() == right.Get();
+}
+
+template <typename Traits>
+bool operator!=(Handle<Traits> const & left, Handle<Traits> const & right) noexcept
+{
+    return !(left == right); 
+}
--- main.cpp
+++ main.cpp
@@ -1,5 +1,33 @@
+#include "sqlite3.h"
 #include <iostream>
+#include "Handle.h"
+
+#ifdef _DEBUG
+#define VERIFY ASSERT
+#define VERIFY_(result, expression) ASSERT(result == expression)
+#else
+#define VERIFY(expression) (expression)
+#define VERIFY_(result, expression) (expression)
+#endif
+
+struct ConnectionHandleTraits : HandleTraits<sqlite3 *>
+{
+    static void Close(Type value) noexcept
+    {
+        VERIFY_(SQLITE_OK, sqlite3_close(value));
+    }
+};
+
+using ConnectionHandle = Handle<ConnectionHandleTraits>;
 
 int main(int, char**) {
-    std::cout << "Hello, world!\n";
+    ConnectionHandle connection;
+
+    int result = sqlite3_open(":memory:", connection.Set());
+
+    if (SQLITE_OK != result)
+    {
+        std::cout << sqlite3_errmsg(connection.Get());
+        return result;
+    }
 }
