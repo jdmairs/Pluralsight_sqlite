06c31901a97521dbfc0b8e8e2bc962630cd184b2
John Mairs <John.D.Mairs@gmail.com>
2018-08-12 09:00:05 -0400

    remove build

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/jmairs/Documents/Pluralsight_sqlite
-# Build directory: /Users/jmairs/Documents/Pluralsight_sqlite/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
