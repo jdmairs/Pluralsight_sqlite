c49bee484abf4c0d0626675850dea68c3da31dbb
John Mairs <John.D.Mairs@gmail.com>
2018-08-15 08:00:10 -0400

    simply let sqlite make rowid

--- main.cpp
+++ main.cpp
@@ -23,14 +23,14 @@ int main(int, char**) {
     {
         Connection connection = Connection::Memory();
        
-        Execute(connection, "create table Hens (Id integer primary key, Name)");
-        Execute(connection, "insert into Hens (Id, Name) values (?,?)", 101, "Marge");
-        Execute(connection, "insert into Hens (Id, Name) values (?,?)", 102, "Jane");
-        Execute(connection, "insert into Hens (Id, Name) values (?,?)", 103, "Greta");
+        Execute(connection, "create table Hens (Name)");
+        Execute(connection, "insert into Hens (Name) values (?)", "Marge");
+        Execute(connection, "insert into Hens (Name) values (?)",  "Jane");
+        Execute(connection, "insert into Hens (Name) values (?)",  "Greta");
 
-        for (Row row : Statement(connection, "select RowId, Id, Name from Hens"))
+        for (Row row : Statement(connection, "select RowId, Name from Hens"))
         {
-            std::cout << "rowid=" << row.GetInt(0) << " " << row.GetInt(1) << " " << row.GetString(2) << "\n";
+            std::cout << "rowid=" << row.GetInt(0) << " " << row.GetString(1) << "\n";
         }
     }
     catch (Exception const &e)
